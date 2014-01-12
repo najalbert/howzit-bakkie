8219ee1dbd9ca9a4a262ab0542d084335acb187b
Nick Jalbert <nickj@captricity.com>
2014-01-12 07:55:35 +0200

    generalize to any player card count

--- blackjack_better.py
+++ blackjack_better.py
@@ -1,17 +1,17 @@
 import random
 
-DEBUG = 1
+DEBUG = 0
 
 DECK_COUNT = 5
-TRIALS = 100
+TRIALS = 10000
 HIT = 'H'
 STAND = 'S'
 
-def simulate_hands(player1, player2, dealer_up):
+def simulate_hands(player_cards, dealer_up):
     DECK_BASE = [2,3,4,5,6,7,8,9,10,11]
     deck = DECK_BASE * 4 * DECK_COUNT 
-    deck.remove(player1)
-    deck.remove(player2)
+    for card in player_cards:
+        deck.remove(card)
     deck.remove(dealer_up)
     hit_hands = {}
     stand_bets = 0
@@ -21,24 +21,25 @@ def simulate_hands(player1, player2, dealer_up):
         next_card = 0
         strategy = random.choice([HIT, STAND])
         if strategy == HIT:
-            hand = (player1, player2, deck[0])
+            hand = tuple(sorted(player_cards + (deck[0],)))
             hit_hands.setdefault(hand, 0)
             hit_hands[hand] += 1
         elif strategy == STAND:
-            stand_bets = 1
-            stand_wins += score_hand(player1, player2, dealer_up, deck)
+            stand_bets += 1
+            stand_wins += score_hand(player_cards, dealer_up, deck)
         else:
             raise Exception('Did you forget something?')
+    print 'Expected value (over %s trials) of standing on %s %s with a dealer up card of %s and a 1 unit bet on the table is %s' % (TRIALS, score_cards(player_cards), player_cards, dealer_up, float(stand_wins)/stand_bets)
 
-def score_hand(player1, player2, dealer_up, deck):
+def score_hand(player_cards, dealer_up, deck):
     if dealer_up + deck[0] == 21:
         wins = 0
-        if DEBUG: print 'Player has %s ([%s, %s]) vs Dealer natural ([%s, %s]): win %s' % (score_player(player1, player2), player1, player2, dealer_up, deck[0], wins)
+        if DEBUG: print 'Player has %s %s vs Dealer natural ([%s, %s]): win %s' % (score_cards(player_cards), player_cards, dealer_up, deck[0], wins)
         return wins
     next_card_index = 1
     while score_dealer(dealer_up, next_card_index, deck) < 17:
         next_card_index += 1
-    player_score = score_player(player1, player2)
+    player_score = score_cards(player_cards)
     dealer_score = score_dealer(dealer_up, next_card_index, deck)
     if player_score > dealer_score and player_score < 22:
         wins = 2
@@ -55,15 +56,11 @@ def score_hand(player1, player2, dealer_up, deck):
     elif player_score < dealer_score and player_score >= 22 and dealer_score >= 22:
         wins = 0
 
-    if DEBUG: print 'Player has %s ([%s, %s]) vs Dealer with %s (%s): win %s' % (player_score, player1, player2, dealer_score, [dealer_up] + deck[:next_card_index], wins)
+    if DEBUG: print 'Player has %s %s vs Dealer with %s (%s): win %s' % (player_score, player_cards, dealer_score, [dealer_up] + deck[:next_card_index], wins)
     return wins
 
-def score_player(player1, player2):
-    if player1 == 11 and player2 == 11:
-        return 12
-    return player1 + player2
-
 def score_dealer(dealer_up, end_of_dealer_hand, deck):
+    return score_cards((dealer_up,) + tuple(deck[:end_of_dealer_hand]))
     total_score = dealer_up
     ace_count = 1 if dealer_up == 11 else 0
     for i in range(end_of_dealer_hand):
@@ -76,7 +73,19 @@ def score_dealer(dealer_up, end_of_dealer_hand, deck):
             total_score -= 10
     return total_score
 
+def score_cards(cards):
+    ace_count = 0
+    total_score = 0
+    for card in cards:
+        if card == 11:
+            ace_count += 1
+        total_score += card
+    for i in range(ace_count):
+        if total_score > 21:
+            total_score -= 10
+    return total_score
 
 
-simulate_hands(7,8,6)
+
+simulate_hands((10,11,11,11,11,11,11,2),10)
 
